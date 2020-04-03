e00934a895130f81b643e6caa01bdf3185faa290
Sushma <varma.sushma1998@gmail.com>
2020-04-03 17:38:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hiii Sushma1!!!
+         Hiii Welcome to My React App!!!
         </p>
         <a
           className="App-link"
